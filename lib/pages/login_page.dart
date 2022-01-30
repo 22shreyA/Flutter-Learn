1173ac00a74f4b354150ff66846272335b3ac89e
Shreya Seth <sks.220801@gmail.com>
2022-01-30 17:05:20 +0530

    day8 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -16,110 +16,111 @@ class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool changeButton = false;
 
+  final _formKey = GlobalKey<FormState>();
+  
+  moveToHome(BuildContext context) async{
+    if(_formKey.currentState!.validate()){
+      setState(() {
+          changeButton = true;
+      });          
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+          changeButton = false;
+      });
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
        child: SingleChildScrollView(
-         child: Column(
-           children: [
-            //  Container(
-            //    height: 300,
-            //    width: 500,
-            //    alignment: Alignment.topCenter,
-            //    decoration: const BoxDecoration(
-            //       image: DecorationImage(image: AssetImage("assets/images/login_image.png"),
-            //       fit: BoxFit.fill),
-            //    ),
-            //  ),
-            Image.asset(
-              "assets/images/login_image.png",
-              fit: BoxFit.fill,
-              height: 300,
-              width: 300,
-            ),
-              SizedBox(
-               height: 10.0,
-             ),
-              Text(
-               "Welcome $name",
-                style: TextStyle(
-                   fontSize: 25,
-                   fontWeight: FontWeight.bold,
-                   color: Colors.cyan,  
+         child: Form(
+           key: _formKey,
+           child: Column(
+             children: [
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.fill,
+                height: 300,
+                width: 300,
+              ),
+                SizedBox(
+                 height: 10.0,
+               ),
+                Text(
+                 "Welcome $name",
+                  style: TextStyle(
+                     fontSize: 25,
+                     fontWeight: FontWeight.bold,
+                     color: Colors.cyan,  
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-             Padding(
-               padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 32.0),
-               child: Column(children: [
+                SizedBox(
+                  height: 10.0,
+                ),
+               Padding(
+                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 32.0),
+                 child: Column(children: [
+                   TextFormField(
+                   decoration: InputDecoration(
+                     hintText: "Enter Username ",
+                     labelText: "Username",
+                     hintStyle: TextStyle(
+                       fontSize: 10,
+                     )
+                   ),
+                   validator: (value) {
+                     return value!.isEmpty ? "Username cannot be empty" : null;
+                   },
+                    onChanged: (value){
+                     name = value;
+                     setState(() { });
+                   },
+                 ),
                  TextFormField(
-                 decoration: InputDecoration(
-                   hintText: "Enter Username ",
-                   labelText: "Username",
-                   hintStyle: TextStyle(
-                     fontSize: 10,
-                   )
-                 ),
-                  onChanged: (value){
-                   name = value;
-                   setState(() { });
-                 },
-               ),
-               TextFormField(
-                 obscureText: true,
-                 decoration: InputDecoration(
-                   hintText: "Enter Password ",
-                   labelText: "Password",
-                   hintStyle: TextStyle(
-                     fontSize: 10,
-                   )
-                 ),
-               ),
-               const SizedBox(
-                  height: 30.0,
-               ),
-              //  ElevatedButton(
-              //    onPressed:() {
-              //       Navigator.pushNamed(context, MyRoutes.homeRoute);
-              //    }, 
-              //    child: Text ("Login"),
-              //    style: TextButton.styleFrom(minimumSize: Size(120, 40)),
-              //   ),
-              InkWell(
-                onTap: () async{
-                  setState(() {
-                    changeButton = true;
-                  });
-
-                  await Future.delayed(Duration(seconds: 1));
-                   Navigator.pushNamed(context, MyRoutes.homeRoute);
-                },
-                child: AnimatedContainer( 
-                  duration: Duration(seconds: 1),
-                   height : 40,
-                   width : changeButton ? 40 : 120,
-                   alignment: Alignment.center,
-                   child: changeButton ? Icon(Icons.done, color: Colors.white,) : Text(
-                      "Login",
-                      style: TextStyle(
-                         fontWeight: FontWeight.normal,
-                         fontSize: 15,
-                      ),
+                   obscureText: true,
+                   decoration: InputDecoration(
+                     hintText: "Enter Password ",
+                     labelText: "Password",
+                     hintStyle: TextStyle(
+                       fontSize: 10,
+                     )
                    ),
-                   decoration: BoxDecoration(
-                     color: Colors.cyan,
-                    //  shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
-                     borderRadius: BorderRadius.circular(changeButton ? 40 : 10),
-                   ),
+                   validator: (value) {
+                     return value!.isEmpty ? "Password cannot be empty" : (value.length < 6 ? "Password length should be atleast 6 " : null );
+                   },
                  ),
-              )
-               ],),
-             )
-       
-           ],
+                 const SizedBox(
+                    height: 30.0,
+                 ),
+                Material(
+                  color: Colors.cyan,
+                  borderRadius: BorderRadius.circular(changeButton ? 40 : 10),
+                  child: InkWell(
+                    splashColor: Colors.blue[900],
+                    onTap: () => moveToHome(context),
+                    child: AnimatedContainer( 
+                      duration: Duration(seconds: 1),
+                       height : 40,
+                       width : changeButton ? 40 : 120,
+                       alignment: Alignment.center,
+                       child: changeButton ? Icon(Icons.done, color: Colors.white,) : Text(
+                          "Login",
+                          style: TextStyle(
+                             fontWeight: FontWeight.normal,
+                             fontSize: 15,
+                          ),
+                       ),
+                     ),
+                  ),
+                )
+              ],),
+            )
+                
+          ],),
          ),
        ));
   }
--- lib/srt_Key.dart
+++ lib/srt_Key.dart
@@ -11,3 +11,26 @@
      always using static is not a good practice.
      make the value of the debugShowCheckedModeBanner as false in main.dart -> to remove debug banner while running
 */
+
+/* day6 
+     learned use of 
+       - 
+       - animated container
+       -  
+     
+*/
+
+/* day7
+     ama and qna session in original....skipped and day 8 is now day 7 branch 
+*/
+
+/* day7
+     Drawback of last day button : Once it is clicked it didn't come back to it's original state
+     Solution : 1. make animatedContainer -> ink (similar to container -> adv = it can give better effects and 
+                   decoration as well as it will go with inkWell too) -------- not optimal becoz we want to use animatedContainer
+                2. wrap inkWell with another widget becoz it need material as ancestor
+                   (Note: whenever we are giving ancestor as material then it's child must not contain decoration)
+
+    ** If we want appbar then we can replace the material widget(root) to scaffold and then place appbar 
+    Learned : how to put repeal effect comes in container, use of form, how to validate on textFields.
+*/   
